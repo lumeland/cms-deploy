0d734a4d07c1333acb07abbd6708c6cf75431830
Óscar Otero <oom@oscarotero.com>
2024-05-15 10:48:20 +0200

    Install git if it's missing

--- install-caddy.sh
+++ install-caddy.sh
@@ -4,7 +4,7 @@
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
-apt install caddy unzip
+apt install caddy unzip git
 curl -fsSL https://deno.land/install.sh | sh
 
 # Ask for required variables
