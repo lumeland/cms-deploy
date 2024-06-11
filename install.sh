81b9a1a3344367c9a45c5da469798b0ed28c1e26
Óscar Otero <oom@oscarotero.com>
2024-06-11 02:05:11 +0200

    code style

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Install packages
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
-apt update
-apt-get install -y caddy unzip git
+apt update -y
+apt install -y git unzip caddy
 curl -fsSL https://deno.land/install.sh | sh
 
 # Ask for required variables
