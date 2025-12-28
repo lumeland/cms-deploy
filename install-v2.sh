5871a495113e34d4d3573c52cbc4f6ed4b6c8b78
Oscar Otero <oom@oscarotero.com>
2025-12-28 22:33:38 +0100

    fixes in the new installation

--- install-v2.sh
+++ install-v2.sh
@@ -2,25 +2,31 @@
 
 # Install and update packages
 apt update -y
-apt install -y git unzip debian-keyring debian-archive-keyring apt-transport-https curl
+apt install -y debian-keyring debian-archive-keyring apt-transport-https curl git unzip
 
-# Install caddy
+# Install Caddy
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 chmod o+r /etc/apt/sources.list.d/caddy-stable.list
-
-# Install xcaddy for a custom build
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-xcaddy-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-xcaddy.list
-chmod o+r /usr/share/keyrings/caddy-xcaddy-archive-keyring.gpg
-chmod o+r /etc/apt/sources.list.d/caddy-xcaddy.list
-
 apt update -y
-apt install caddy xcaddy
+apt install -y caddy
 
-# Build a custom Caddy with Lume plugin
-xcaddy build --with github.com/lumeland/caddy-lume
+# Install custom Caddy with Lume
+if [ $(uname -m) = "x86_64" ]; then
+  target="linux-amd64"
+else
+  target="linux-arm64"
+fi
+binary_url="https://github.com/lumeland/caddy-lume/releases/latest/download/caddy-lume-${target}.tar.gz"
+curl --fail --location --progress-bar --output caddy.tar.gz "$binary_url"
+tar -xf caddy.tar.gz
+mv "bin/caddy-lume-$target" ./caddy
+chmod +x caddy
+rm caddy.tar.gz
+rm -rf bin
+
+# Configure official Caddy and custom Caddy-Lume
 dpkg-divert --divert /usr/bin/caddy.default --rename /usr/bin/caddy
 mv ./caddy /usr/bin/caddy.custom
 update-alternatives --install /usr/bin/caddy caddy /usr/bin/caddy.default 10
@@ -63,6 +69,29 @@ CMS_PASSWORD=
 EOF
 
 echo "File ${dir}/.env created. Please, edit the environment variables"
+
+# Setup Caddy service
+cat > /etc/systemd/system/caddy.service << EOF
+[Unit]
+Description=Caddy
+Documentation=https://caddyserver.com/docs/
+After=network.target network-online.target
+Requires=network-online.target
+
+[Service]
+Type=notify
+User=root
+ExecStart=/usr/bin/caddy run --environ --config /etc/caddy/Caddyfile
+ExecReload=/usr/bin/caddy reload --config /etc/caddy/Caddyfile --force
+TimeoutStopSec=5s
+LimitNOFILE=1048576
+PrivateTmp=true
+ProtectSystem=full
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 # Create Caddyfile
 mkdir -p /etc/caddy
